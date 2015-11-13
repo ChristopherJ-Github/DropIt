a13722f8e8168c9dfb5cffcba81dfd6839033aa6
Chris Jadoo <cj12ih@student.ocadu.ca>
2015-11-13 18:21:19 -0500

    Basic witch shooting added

---
+++ __OriginalAssets/Scripts/Witch.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Witch : MonoBehaviour
+{
+	void Start ()
+    {
+        InvokeRepeating("Attack", 2, 2);
+	}
+
+    public Transform spawnPoint;
+    public GameObject projectile;
+    public float shootForce;
+	
+	void Attack ()
+    {
+        Vector3 position = spawnPoint.position;
+        Vector3 direction = PlayerManager.instance.currentCharacter.transform.position - spawnPoint.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        GameObject projectile = Instantiate(this.projectile, position, rotation) as GameObject;
+        Rigidbody _rigidbody = projectile.GetComponent<Rigidbody>();
+        _rigidbody.AddForce(direction * shootForce);
+    }
+}
