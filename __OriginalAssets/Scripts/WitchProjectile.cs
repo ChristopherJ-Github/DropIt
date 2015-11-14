78d7f0d67f338bdc5390dfaac84591760e5faa51
Chris Jadoo <cj12ih@student.ocadu.ca>
2015-11-13 19:04:40 -0500

    Witch projectiles destroy self after certain amount of time now

---
+++ __OriginalAssets/Scripts/WitchProjectile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WitchProjectile : MonoBehaviour
+{
+	void Start ()
+    {
+        StartCoroutine(DestructionTimer());
+	}
+
+    public float destroyTime;
+
+    IEnumerator DestructionTimer ()
+    {
+        yield return new WaitForSeconds(destroyTime);
+        DestroySelf();
+    }
+	
+	void DestroySelf ()
+    {
+        Destroy(gameObject);
+    }
+}
