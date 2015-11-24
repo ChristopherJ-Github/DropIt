398b8d53ac03f9e925bbe97eb42f6974cbbbf862
Chris Jadoo <cj12ih@student.ocadu.ca>
2015-11-24 16:28:20 -0500

    Basic camera follow script created

--- __OriginalAssets/Scripts/CameraFollow.cs
+++ __OriginalAssets/Scripts/CameraFollow.cs
@@ -4,30 +4,39 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour
 {
     public Transform toFollow;
+    private Transform newTransform;
 
 	void Start ()
     {
-        currentPosition = toFollow.position;
+        newTransform = transform;
 	}
     
     public float positionSpeed;
     public float rotationSpeed;
 
-    void Update ()
+    void FixedUpdate ()
     {
         FollowPosition(positionSpeed);
-        FollowRotation(positionSpeed);
+        FollowRotation(rotationSpeed);
 	}
 
-    private Vector3 currentPosition;
+    public Vector3 positionOffset;
 
     void FollowPosition (float speed)
     {
-        
+        Vector3 targetPosition = toFollow.position + toFollow.TransformVector(positionOffset);
+        Vector3 newPosition = Vector3.Slerp(newTransform.position, targetPosition, speed * Time.deltaTime);
+        newTransform.position = newPosition;
     }
+
+    public Transform planet;
 
     void FollowRotation (float speed)
     {
-
+        Vector3 forward = (planet.position - newTransform.position).normalized;
+        Vector3 up = toFollow.forward;
+        Quaternion targetRotation = Quaternion.LookRotation(forward, up);
+        Quaternion newRotation = Quaternion.Slerp(newTransform.rotation, targetRotation, speed * Time.deltaTime);
+        newTransform.rotation = newRotation;
     }
 }
